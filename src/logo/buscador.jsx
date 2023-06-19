79c5c624878b6d927faae59fcf9deae0c0df9f5c
Benjamin Gutierrez <benjamingutierrez0097@gmail.com>
2023-06-19 03:54:05 -1200

    I finish the opcion to search

--- src/logo/buscador.jsx
+++ src/logo/buscador.jsx
@@ -2,25 +2,45 @@ import React, { useState } from 'react';
 import './buscador.css'
 
 function Buscador() {
-  const [showPopup, setShowPopup] = useState(false);
 
-  const handleInputClick = () => {
-    setShowPopup(true);
+  const [window, setWindow] = useState(false);
+
+  const toggleWindow = () => {
+    setWindow(!window);
+  };
+
+  const handleOutsideClick = () => {
+    setWindow(false);
   };
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Haz clic aquí"
-        onClick={handleInputClick}
-      />
-      {showPopup && (
-        <div className="popup">
-          <input type="text" placeholder="Introduce datos" />
-        </div>
-      )}
-    </div>
+      <button id="logo" onClick={toggleWindow}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+    </svg>
+    </button>
+
+    {window && (
+      <div className="white-window" onClick={toggleWindow}>
+        {
+          <>
+            <div id="windowsearch" onClick={(e) => e.stopPropagation()}>
+                <input id='destiny' placeholder='Place'/>
+                <input id='people' placeholder='Add guest' />
+                <button id='find'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bii bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+                Search
+                </button>
+            </div>
+          </>
+        }
+      </div>
+    )}
+    {window && <div className="overlay" onClick={handleOutsideClick} />} 
+    </div> 
   );
 }
 
